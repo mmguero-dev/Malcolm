2ccf01a8b04a339a311739b45483951492406ef3
Seth Grover <seth.d.grover@gmail.com>
2026-04-20 14:10:15 -0600

    raspi fixes

--- malcolm-iso/config/includes.chroot/usr/local/bin/agg-init.sh
+++ malcolm-iso/config/includes.chroot/usr/local/bin/agg-init.sh
@@ -93,7 +93,7 @@ EOF
 
   # if we need to import prebuilt Malcolm docker images, do so now (but not if we're in a live-usb boot)
   DOCKER_DRIVER="$(docker info -f json 2>/dev/null | jq -r '.Driver')"
-  if [[ -n $DOCKER_DRIVER ]] && [[ "$DOCKER_DRIVER" != "vfs" ]] && ! grep -q boot=live /proc/cmdline; then
+  if [[ -f /malcolm_images.tar.xz ]] && [[ -n $DOCKER_DRIVER ]] && [[ "$DOCKER_DRIVER" != "vfs" ]] && ! grep -q boot=live /proc/cmdline; then
     docker load -q -i /malcolm_images.tar.xz && rm -f /malcolm_images.tar.xz
   fi
 
