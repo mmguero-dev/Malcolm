996379c14114198e44dc00b45962091ecee37f88
Seth Grover <seth.d.grover@gmail.com>
2026-06-09 20:31:08 +0000

    bump yq and fluent bit versions

--- scripts/third-party-logs/fluent-bit-setup.ps1
+++ scripts/third-party-logs/fluent-bit-setup.ps1
@@ -9,7 +9,7 @@
 ###############################################################################
 
 $fluent_bit_version = '5.0'
-$fluent_bit_full_version = '5.0.6'
+$fluent_bit_full_version = '5.0.7'
 
 ###############################################################################
 # select an item from a menu provided in an array
