5acc9d28711fe40058f8c0483019ed809a7dcb03
Seth Grover <seth.d.grover@gmail.com>
2026-08-17 22:52:00 +0000

    fix path name for zeek script branch

--- zeek/scripts/zeek_install_plugins.sh
+++ zeek/scripts/zeek_install_plugins.sh
@@ -150,7 +150,7 @@ ZKG_GITHUB_URLS=(
   "https://github.com/corelight/zerologon"
   "https://github.com/cybera/zeek-sniffpass"
   "https://github.com/FoxIO-LLC/ja4|main"
-  "https://github.com/J-Gras/zeek-fuzzy-hashing|hash-all-files"
+  "https://github.com/J-Gras/zeek-fuzzy-hashing|topic/jgras/hash-all-files"
   "https://github.com/mmguero-dev/bzar"
   "https://github.com/corelight/json-streaming-logs|master"
   "https://github.com/ncsa/bro-is-darknet"
